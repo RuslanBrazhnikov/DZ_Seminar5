b4a9c8018eeb5817a756835640d2b6593561b133
Ruslan <ruslan.1995mai@gmail.com>
2022-08-25 01:02:56 +0500

    Task3

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,6 +1,7 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
-// Напишите программу, которая покажет количество чётных чисел в массиве.
-// [345, 897, 568, 234] -> 2
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
 
 Console.Clear();
 int element = 8;
@@ -10,7 +11,7 @@ void FillArray(int elem, int[] array)
 {
     for (int i = 0; i < elem; i++)
     {
-        array[i] = new Random().Next(100, 1000); 
+        array[i] = new Random().Next(1,9); 
     }
 }
 
@@ -23,14 +24,14 @@ void PrintArray(int elem2, int[] array2)
     }
     Console.Write($"]");
 }
-    
+
 int result = 0;
 
 int GetMass(int count)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i] % 2 == 0) result = result + 1;
+        if (i % 2 != 0) result = result + arr[i];
     }
     return result;
 }
@@ -39,4 +40,4 @@ FillArray(8, arr);
 int res = GetMass(result);
 PrintArray(8, arr);
 Console.WriteLine();
-Console.WriteLine($"Количество четных чисел в массиве = {result}");
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях = {result}");
---
+++ Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементами массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+Console.Clear();
+double[] arr = new double[8];
+double max = 0;
+double min = 9999;
+
+
+
+void FillArray(double elem, double[] array)
+{
+    for (int i = 0; i < elem; i++)
+    {
+        array[i] = new Random().Next(10, 90) / 10.0;
+    }
+}
+
+double GetMaxMin()
+{
+    for (int i = 0; i < arr.Length; i++) {
+
+        if (arr[i] > max) {
+            max = arr[i];
+        } 
+        if (arr[i] < min)  {
+            min = arr[i];
+        }
+    }
+
+    double res = max - min;
+
+    return res;
+}
+
+void PrintArray(double elem2, double[] array2)
+{
+    Console.Write($"array = [ ");
+    for (int i = 0; i < elem2; i++)
+    {
+        Console.Write($"{array2[i]}, ");
+    }
+    Console.Write($"]");
+}
+
+FillArray(8, arr);
+PrintArray(8, arr);
+double result = Math.Round(GetMaxMin(), 2);
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива = {result}");
